654512091058501b2a89936e176b6d934f010ef6
Allan Arnesen <allan.arnesen@gmail.com>
2017-11-11 17:33:44 +0100

    Some renaming and init of SeedData.

---
+++ HiN-Ventures/Models/SeedData.cs
@@ -0,0 +1,46 @@
+﻿using HiN_Ventures.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiN_Ventures.Models
+{
+    public class SeedData
+    {
+        public static async Task InitializeAsync(ApplicationDbContext context, UserManager<ApplicationUser> uM, RoleManager<IdentityRole> rM)
+        {
+
+            await createRolesAsync(context, rM);
+            await createUsersAsync(context, uM);
+        }
+
+        private static async Task createUsersAsync(ApplicationDbContext context, UserManager<ApplicationUser> uM)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = "admin@test.com",
+                Email = "admin@test.com"
+            };
+
+            await uM.CreateAsync(user);
+            var cU = await uM.FindByEmailAsync(user.Email);
+            await uM.AddPasswordAsync(user, "passord1@admin");
+            await uM.AddToRoleAsync(user, "Administrator");
+            await context.SaveChangesAsync();
+        }
+
+        private static async Task createRolesAsync(ApplicationDbContext context, RoleManager<IdentityRole> rM)
+        {
+            await rM.CreateAsync(new IdentityRole("Administrator"));
+            await context.SaveChangesAsync();
+            await rM.CreateAsync(new IdentityRole("Senior"));
+            await context.SaveChangesAsync();
+            await rM.CreateAsync(new IdentityRole("Freelance"));
+            await context.SaveChangesAsync();
+            await rM.CreateAsync(new IdentityRole("Klient"));
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- HiN-Ventures/Program.cs
+++ HiN-Ventures/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using HiN_Ventures.Models;
+using Microsoft.Extensions.DependencyInjection;
+using HiN_Ventures.Data;
 
 namespace HiN_Ventures
 {
@@ -14,7 +18,26 @@ namespace HiN_Ventures
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    SeedData.InitializeAsync(context, userManager, roleManager).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
+            
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
