640f4ba47e749592b056520fe5fe36f71d25dc9f
Benedicte Karlsen <bka049@post.uit.no>
2017-11-23 14:46:25 +0100

    Moq installert

--- HiN_Ventures_UnitTests/FreelanceControllerTest.cs
+++ HiN_Ventures_UnitTests/FreelanceControllerTest.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace HiN_Ventures_UnitTests
 {
     [TestClass]
     public class FreelanceControllerTest
     {
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+
         [TestMethod]
         public void RegisterTest1()
         {
+            // Arrange
+
+
+            // Act
+
+            // Assert
         }
     }
 }
